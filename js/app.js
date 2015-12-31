e5a83d88e7aa8ff6c1354226375e08b266c81e08
grimal <phillip@philliprstafford.com>
2015-12-31 15:14:04 -0500

    Add drawer to unordered list. Add Hamburger SVG. Adjust map location. Add map style.  Add header.

--- js/app.js
+++ js/app.js
@@ -365,12 +365,121 @@
 		initMap: function() {
 			var stamford = new google.maps.LatLng(41.074448, -73.541316);
 
+			  // Specify features and elements to define styles.
+			  var styleArray = [
+			    {
+			        "featureType": "administrative",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road",
+			        "elementType": "labels",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.local",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "stylers": [
+			            {
+			                "color": "#84afa3"
+			            },
+			            {
+			                "lightness": 52
+			            }
+			        ]
+			    },
+			    {
+			        "stylers": [
+			            {
+			                "saturation": -17
+			            },
+			            {
+			                "gamma": 0.36
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit.line",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#3f518c"
+			            }
+			        ]
+			    }
+			  ];
+
 			// Create a map object and specify the DOM element dor display.
 			this.map = new google.maps.Map(document.getElementById('map'), {
 				center: stamford,
 				mapTypeId: google.maps.MapTypeId.ROADMAP,
 				scroolwheel: true,
-				zoom: 15
+				zoom: 15,
+				styles: styleArray
 			});
 		}
 	};
@@ -388,10 +497,12 @@
 				number: place.number(),
 				label: place.marker(),
 				animation: google.maps.Animation.DROP
-			})
+			});
 
 			self.markers.push(marker);
 			marker.setMap(myMap);
+
+
 	};
 
 
@@ -520,9 +631,10 @@
 			// 	content: place.description
 			// });
 
-			// marker.addListener('click', function() {
-			// 	infoWindow.open(Map.map, marker);
-			// });
+			marker.addListener('click', function() {
+				focusMarker(place);
+				//infoWindow.open(Map.map, marker);
+			});
 
 		 //    google.maps.event.addListener(marker, 'rightclick', function(event) {
 		 //        marker.setMap(null);
@@ -582,12 +694,16 @@
 				self.markers[self.currentPlace].setIcon('');
 			}
 
-			obtainWiki(place.name);
+			//obtainWiki(place.name);
 
 			self.currentPlace = place.number;
 			self.infoWindow.setOptions({
 				content: place.description
 			});
+			// console.log(Map);
+			// var chicago = new google.maps.LatLng(place.lat, place.lng);
+ 		// 	Map.setCenter(chicago);
+			//Map.setCenter({lat: place.lat, lng: place.lng});
 
 			self.infoWindow.open(Map.map, self.markers[place.number]);
 			// Chnage the selected Marker icon to green.
